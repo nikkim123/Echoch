dbc3548066dae3fc3536044db7e07a29d2edca5c
neethu <neethupushpan01@gmail.com>
2018-04-04 10:55:02 +0530

    cm7

---
+++ EchoClient.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoClient
+{
+public static void main(String args[]) throws Exception
+{
+
+try {
+int Port;
+
+BufferedReader Buf =new BufferedReader(new
+InputStreamReader(System.in));
+System.out.print(" Enter the Port Address : " );
+Port=Integer.parseInt(Buf.readLine());
+Socket sok=new Socket("localhost",Port);
+
